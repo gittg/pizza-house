5fa2061d3400ff230095a7975263ed138c0aefb4
Ivan <githubh@gmail.com>
2016-02-19 16:26:11 +0200

    Add localstorage variables

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
-  function something() {
-    alert ('hello!');
+  function something() 
+  {
+    var x =  window.localStorage.getItem('aaa');
+    window.localStorage.setItem('aaa', 555);
+    alert(x);
   }
