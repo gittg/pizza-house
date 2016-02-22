fac20036106e0eb5f0b5df09e55f3ea9e1ca67be
Ivan <githubh@gmail.com>
2016-02-22 22:12:04 +0200

    Add function cart_get_orders

--- public/app.js
+++ public/app.js
@@ -33,3 +33,20 @@
       }
       return cnt;
   }
+
+    function cart_get_orders()
+  {
+      var orders = '';
+
+      for(var i = 0; i < window.localStorage.length; i++)
+      {
+          var key = window.localStorage.key(i);
+          var value = window.localStorage.getItem(key);
+
+          if(key.indexOf('product_') == 0)
+          {
+              orders = orders + key + '=' + value + ',';
+          }
+      }
+      return orders;
+  }
