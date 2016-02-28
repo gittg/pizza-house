f9243278a30d4833d6e5c45ee70c04890857c5d7
Ivan <githubh@gmail.com>
2016-02-28 20:43:12 +0200

    Update cart function

--- public/app.js
+++ public/app.js
@@ -69,5 +69,6 @@ function update_orders_button ()
       window.localStorage.clear();
           update_orders_input();
           update_orders_button ();
+          $('#cart').text('Your cart is now empty');
       return false;
   }
