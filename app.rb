74aae5a43cd9228a8dd2f9d83e0a09ff0005b0a0
Ivan <githubh@gmail.com>
2016-02-28 13:42:39 +0200

    Update cart.erb

--- app.rb
+++ app.rb
@@ -30,23 +30,23 @@ post '/cart' do
 end
 
 def parse_orders_input orders_input
+  s1 = orders_input.split(/,/)
 
-	s1 = orders_input.split(/,/)
-	arr = Array.new
+  arr = []
 
-	s1.each do |x|
-		s2 = x.split(/\=/)
-		s3 = s2[0].split(/_/)
+  s1.each do |x|
+    s2 = x.split(/\=/)
 
-		id = s3[1]
-		cnt = s2[1]
+    s3 = s2[0].split(/_/)
 
-		arr2 = [id, cnt]
+    id = s3[1]
+    cnt = s2[1]
 
-		arr.push arr2
+    arr2 = [id, cnt]
 
-	end
+    arr.push arr2
+  end
 
-	return arr
+  return arr
 end
 
